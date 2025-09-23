8a5b1655fba17f6d294ced88fd8fa6eeb51869b9
Carryours <Carryours@aliyun.com>
2025-09-23 10:45:11 +0800

    test: update test case

--- src/sign/typeC.ts
+++ src/sign/typeC.ts
@@ -8,11 +8,14 @@ const GenTypeCUrl = function (
 ): string {
   const defaultTs = Math.floor(Date.now() / 1000);
   const params = splitUrl(url);
-  const scheme = params[0], host = params[1], path = params[2], args = params[3];
+  const scheme = params[0],
+    host = params[1],
+    path = params[2],
+    args = params[3];
   const tsStr = ts ? ts.toString(16) : defaultTs.toString(16);
   const text = `${key}${path}${tsStr}`;
   const hashVal = hash(algorithm, text);
-  return `${scheme}${host}/${hashVal}/${tsStr}${path}${args ? args : ''}`;
+  return `${scheme}${host}/${hashVal}/${tsStr}${path}${args ? args : ""}`;
 };
 
 export default GenTypeCUrl;
